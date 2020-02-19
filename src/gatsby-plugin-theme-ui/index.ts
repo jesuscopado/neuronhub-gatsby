cc7fa6d8ab63e612e1c3ac9b49d1cba6896e0227
jesuscopado <jcr.22@hotmail.com>
2020-02-20 00:35:59 +0200

    Loaded most of the plus + changes of style such as distance between paragraph and image hero removed from article

---
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -0,0 +1,12 @@
+import novelaTheme from '@narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui';
+
+export default {
+  ...novelaTheme,
+  fonts: {
+    ...novelaTheme.fonts,
+    serif: "'Merriweather', Georgia, Serif",
+    sansSerif:
+      "'Harding', 'SF Pro Display', '-apple-system', 'BlinkMacSystemFont', 'San Francisco', 'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI', 'Arial', sans-serif",
+    monospace: `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
+  },
+};
