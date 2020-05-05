dc7f538c285a036619ea55745523ccbff9a3dc59
jesuscopado <jcr.22@hotmail.com>
2020-05-06 02:40:05 +0300

    erased console logs

--- static/collapse_section.js
+++ static/collapse_section.js
@@ -1,19 +1,14 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-console.log("YO!!");
-console.log(coll.length);
-
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.parentElement.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-      console.log("if");
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-      console.log(content.scrollHeight);
     }
   });
 }
