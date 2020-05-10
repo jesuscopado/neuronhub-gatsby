1199ae3c3720f84f428faa89850e84a71d6af8b0
jesuscopado <jcr.22@hotmail.com>
2020-05-10 13:12:36 +0300

    Test with addEventListener

--- static/collapse_section.js
+++ static/collapse_section.js
@@ -1,19 +1,20 @@
-console.log('22');
+console.log('before event loader');
+window.addEventListener('load', (event) => {
+  console.log('The page has fully loaded');
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
-console.log('The page has fully loaded');
-var coll = document.getElementsByClassName("collapsible");
-var i;
+  console.log(coll.length);
 
-console.log(coll.length);
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.parentElement.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.parentElement.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+})
