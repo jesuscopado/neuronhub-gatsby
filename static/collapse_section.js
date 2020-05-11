18e3cc25e0c2a6ef229b6bfc49a670da6eb94f6c
jesuscopado <jcr.22@hotmail.com>
2020-05-11 13:16:52 +0300

    Fixed references collapsible

--- static/collapse_section.js
+++ static/collapse_section.js
@@ -1,3 +1,20 @@
+// in case the reader access the article through the main page
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.parentElement.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+// in case the reader access the article through a direct link
 window.addEventListener('load', (event) => {
   var coll = document.getElementsByClassName("collapsible");
   var i;
