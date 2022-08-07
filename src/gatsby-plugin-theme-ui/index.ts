158459092590b68cfd7c130c1c206ae21fca0912
jesuscopado <jcr.22@hotmail.com>
2022-08-07 20:32:57 +0200

    Dark mode accent changed to pink

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -14,5 +14,26 @@ export default {
     ...novelaTheme.colors,
     accent: "#bb3f5c",
     accentLight: "#f6e7eb",
+    modes: {
+      dark: {
+        grey: "#73737D",
+        primary: "#fff",
+        secondary: "#fff",
+        accent: "#bb3f5c",
+        background: "#111216",
+        hover: "rgba(255, 255, 255, 0.07)",
+        gradient:
+          "linear-gradient(180deg, #111216 0%, rgba(66, 81, 98, 0.36) 100%)",
+        articleText: "#fff",
+        track: "rgba(255, 255, 255, 0.3)",
+        progress: "#fff",
+        card: "#1D2128",
+        error: "#EE565B",
+        success: "#46B17B",
+        errorBackground: "rgba(238, 86, 91, 0.1)",
+        horizontalRule: "rgba(255, 255, 255, 0.15)",
+        inputBackground: "rgba(255, 255, 255, 0.07)",
+      },
+    },
   },
 };
